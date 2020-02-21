c0856fd1374b01ad15be3fe73730068f16baa009
Philip McKeown <pmckeown@deloitte.co.uk>
2020-02-21 15:08:26 +0000

    adding Java cert docs

---
+++ src/myClasses/Dog.java
@@ -0,0 +1,20 @@
+package myClasses;
+
+public class Dog {
+    String breed;
+    int ageC;
+    String Colour;
+
+    void barking() {
+
+    }
+
+    void hungry() {
+
+    }
+
+    void sleeping() {
+
+    }
+
+}
